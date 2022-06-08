dcf9bcd91bf04138d7f45655c823320ea8aee435
TondaNosov <nosov.anton.v@gmail.com>
2022-06-08 16:55:57 +0200

    Test mobile browser Chrome 5

--- sources/js/index.js
+++ sources/js/index.js
@@ -1,34 +1,34 @@
 /* OPEN / CLOSE NAV */
 
-let navToggle = document.querySelector('.main-nav__toggle');
-let navBar = document.querySelector('.main-nav');
+// let navToggle = document.querySelector('.main-nav__toggle');
+// let navBar = document.querySelector('.main-nav');
 
-navToggle.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  if (navBar.classList.contains('main-nav--close')) {
-    navBar.classList.remove('main-nav--close');
-    navBar.classList.add('main-nav--open');
-  } else {
-    navBar.classList.remove('main-nav--open');
-    navBar.classList.add('main-nav--close');
-  };
-});
+// navToggle.addEventListener("click", function (evt) {
+//   evt.preventDefault();
+//   if (navBar.classList.contains('main-nav--close')) {
+//     navBar.classList.remove('main-nav--close');
+//     navBar.classList.add('main-nav--open');
+//   } else {
+//     navBar.classList.remove('main-nav--open');
+//     navBar.classList.add('main-nav--close');
+//   };
+// });
 
 /* NAV POSITION */
 
-let header = document.querySelector(".page-header");
+// let header = document.querySelector(".page-header");
 
-document.addEventListener("scroll", function() {
-  if (document.documentElement.scrollTop > (header.clientHeight + navBar.clientHeight)) {
-   document.querySelector(".main-nav").classList.add("main-nav--fixed"),
-   document.querySelector(".main-nav").classList.add("main-nav--down");
-  }
+// document.addEventListener("scroll", function() {
+//   if (document.documentElement.scrollTop > (header.clientHeight + navBar.clientHeight)) {
+//    document.querySelector(".main-nav").classList.add("main-nav--fixed"),
+//    document.querySelector(".main-nav").classList.add("main-nav--down");
+//   }
 
-  if (document.documentElement.scrollTop < header.clientHeight) {
-    document.querySelector(".main-nav").classList.remove("main-nav--fixed"),
-    document.querySelector(".main-nav").classList.remove("main-nav--down");
-   }
- });
+//   if (document.documentElement.scrollTop < header.clientHeight) {
+//     document.querySelector(".main-nav").classList.remove("main-nav--fixed"),
+//     document.querySelector(".main-nav").classList.remove("main-nav--down");
+//    }
+//  });
 
 /* ACTIVE PAGE */
 
